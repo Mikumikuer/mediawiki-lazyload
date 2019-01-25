e0ed1bb4be4fc16d6ef6b78ab7b286690564ea89
Mikumikuer <46742533+Mikumikuer@users.noreply.github.com>
2019-01-25 17:00:21 +0800

    added parameters to disable lazyload
    add class 'no-lazy' to file, eg:[[file:xx|class=no-lazy]] to disable Lazyload for certain image. In case of XHRequset makes lazyload broke.

--- Lazyload.class.php
+++ Lazyload.class.php
@@ -16,12 +16,14 @@ class Lazyload {
         global $wgRequest, $wgTitle;
         if (defined('MW_API') && $wgRequest->getVal('action') === 'parse') return true;
         if (isset($wgTitle) && $wgTitle->getNamespace() === NS_FILE) return true;
-        $attribs['data-url'] = $attribs['src'];
-        $attribs['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-        if (isset($attribs['srcset'])) {
-            $attribs['data-srcset'] = $attribs['srcset'];
-            unset($attribs['srcset']);
-        }
+        if ( strpos( $attribs['class'], 'no-lazy' ) !== true) {//add class 'no-lazy' to file, eg:[[file:xx|class=no-lazy]] to disable Lazyload for certain image
+		$attribs['data-url'] = $attribs['src'];
+		$attribs['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+		if (isset($attribs['srcset'])) {
+			$attribs['data-srcset'] = $attribs['srcset'];
+			unset($attribs['srcset']);
+		    }
+	    }
         return true;
     }
 
