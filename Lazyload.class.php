c15c972c97264cdc154957095f24928c3083cf5f
mikumikuer <mikumikuer@gmail.com>
2019-01-25 17:43:58 +0800

    Fix bugs

--- Lazyload.class.php
+++ Lazyload.class.php
@@ -16,14 +16,13 @@ class Lazyload {
         global $wgRequest, $wgTitle;
         if (defined('MW_API') && $wgRequest->getVal('action') === 'parse') return true;
         if (isset($wgTitle) && $wgTitle->getNamespace() === NS_FILE) return true;
-        if ( strpos( $attribs['class'], 'no-lazy' ) !== true) {//add class 'no-lazy' to file, eg:[[file:xx|class=no-lazy]] to disable Lazyload for certain image
-		$attribs['data-url'] = $attribs['src'];
-		$attribs['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-		if (isset($attribs['srcset'])) {
-			$attribs['data-srcset'] = $attribs['srcset'];
-			unset($attribs['srcset']);
-		    }
-	    }
+        if ( isset($attribs['class']) && strpos( $attribs['class'], 'no-lazy' ) !== false) return true;
+	$attribs['data-url'] = $attribs['src'];
+	$attribs['src'] = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+	if (isset($attribs['srcset'])) {
+		$attribs['data-srcset'] = $attribs['srcset'];
+		unset($attribs['srcset']);
+		}
         return true;
     }
 
